a4319d40ab1b8d1e8b97e55ad79571bb1b3298e2
Bill Jia <songqi.jia@mail.mcgill.ca>
2022-11-11 11:38:12 -0500

    finished a6

--- assignment_6/numpy_test.py
+++ assignment_6/numpy_test.py
@@ -1,20 +1,19 @@
 import numpy as np
 from matplotlib import pyplot as plt
 n=100000
-# print(n)
 x=np.arange(n)
 x[n//2:]=x[n//2:]-n
 
-# kernel=np.exp(-0.5*(x**2)/(10**2)) #make a Gaussian kernel
-# plt.plot(kernel)
-# plt.show()
+kernel=np.exp(-0.5*x**2/10**2) #make a Gaussian kernel
+plt.plot(kernel)
+plt.show()
 
-y = np.exp(-0.5*(x[50000]**2)/(10**2))
-print(y)
-print(x[50000])
-print(-0.5*(x[50000]**2)/(10**2))
-print(x[50000]**2)
-print((-50000)**2)
+# y = np.exp(-0.5*((x[50000]/10)**2))
+# print(y)
+print('value at midpoint is:',x[50000])
+print('type of the array is:',type(x[50000]))
+print('squared value is:',x[50000]**2)
+print('true square value is:',(-50000)**2)
 
 # x=-50000
 # print(np.exp(-0.5*((x**2)/(10**2))))
--- assignment_6/q5.py
+++ assignment_6/q5.py
@@ -58,12 +58,10 @@ event_name.append('GW170104')
 
 def smooth_vector(vec,sig):
     n=len(vec)
-    x=np.arange(n)
+    x=np.arange(0,n,1.0)
     x[n//2:]=x[n//2:]-n
     kernel=np.exp(-0.5*((x/sig)**2)) #make a Gaussian kernel
     kernel=kernel/kernel.sum()
-    plt.plot(kernel)
-    plt.show()
     vecft=np.fft.rfft(vec)
     kernelft=np.fft.rfft(kernel)
     vec_smooth=np.fft.irfft(vecft*kernelft) #convolve the data with the kernel
@@ -89,6 +87,7 @@ def ligo_analysis(n):
     print('reading file ',hname)
     strain_h,dt_h,utc_h=read_file(hname)
     strain_l,dt_l,utc_l=read_file(lname)
+    ename = event_name[n]
 
     template_name=fname_temp[n]
     tp,tx=read_template(template_name)
@@ -101,6 +100,7 @@ def ligo_analysis(n):
     noise_ft_h=np.fft.fft(win*strain_h)
     noise_smooth_h=smooth_vector(np.abs(noise_ft_h)**2,10)
     noise_smooth_h=noise_smooth_h[:len(noise_ft_h)//2+1] #will give us same length
+
 
     noise_ft_l=np.fft.fft(win*strain_l)
     noise_smooth_l=smooth_vector(np.abs(noise_ft_l)**2,10)
@@ -119,21 +119,23 @@ def ligo_analysis(n):
     Ninv_l[nu>1500]=0
     Ninv_l[nu<20]=0
 
-    template_ft_white_h=np.fft.rfft(tp*win)/np.sqrt(Ninv_h)
-    data_ft_white_h=np.fft.rfft(strain_h*win)/np.sqrt(Ninv_h)
+    template_ft_white_h=np.fft.rfft(tp*win)*np.sqrt(Ninv_h)
+    data_ft_white_h=np.fft.rfft(strain_h*win)*np.sqrt(Ninv_h)
     template_white_h = np.fft.irfft(template_ft_white_h)
     data_white_h = np.fft.irfft(data_ft_white_h)
     rhs_h = np.fft.irfft(data_ft_white_h*np.conj(template_ft_white_h))
-    lhs_h = np.sum(data_white_h**2)
+    lhs_h = np.dot(template_white_h,template_white_h)
     m_h = rhs_h/lhs_h
+    m_h_shifted = np.fft.fftshift(m_h)
 
-    template_ft_white_l=np.fft.rfft(tp*win)/np.sqrt(Ninv_l)
-    data_ft_white_l=np.fft.rfft(strain_l*win)/np.sqrt(Ninv_l)
+    template_ft_white_l=np.fft.rfft(tp*win)*np.sqrt(Ninv_l)
+    data_ft_white_l=np.fft.rfft(strain_l*win)*np.sqrt(Ninv_l)
     template_white_l = np.fft.irfft(template_ft_white_l)
     data_white_l = np.fft.irfft(data_ft_white_l)
     rhs_l = np.fft.irfft(data_ft_white_l*np.conj(template_ft_white_l))
-    lhs_l = np.sum(data_white_l**2)
+    lhs_l = np.sum(template_white_l**2)
     m_l = rhs_l/lhs_l
+    m_l_shifted = np.fft.fftshift(m_l)
 
     signal_height_h = np.max(np.abs(m_h))
     num_noise_h = np.std(m_h)
@@ -147,8 +149,8 @@ def ligo_analysis(n):
     num_SNR_l = signal_height_l/num_noise_l
     ana_SNR_l = signal_height_l/ana_noise_l
 
-    num_SNR_total = np.sqrt(num_SNR_h**2 + num_SNR_l**2)
-    ana_SNR_total = np.sqrt(ana_SNR_h**2 + ana_SNR_l**2)
+    num_SNR_total = 0.5*(np.sqrt(num_SNR_h)+ np.sqrt(num_SNR_l))**2
+    ana_SNR_total = 0.5*(np.sqrt(ana_SNR_h)+ np.sqrt(ana_SNR_l))**2
 
     weight_h = np.abs(template_ft_white_h)**2
     weight_l = np.abs(template_ft_white_l)**2
@@ -156,20 +158,41 @@ def ligo_analysis(n):
     half_v_l = np.average(nu,weights = weight_l)
 
     t_diff = np.abs(np.argmax(np.abs(m_h))-np.argmax(np.abs(m_l)))*dt_h
-    t_diff_var = np.sqrt(2)*20*dt_h
+    t_diff_var = np.sqrt(2)*10*dt_h
     angle = 3e5*t_diff/1e3 + np.pi
     angle_var = 3e5*t_diff_var/1e3
 
 
-    plt.plot(noise_smooth_h)
-    plt.show()
-    plt.plot(m_h)
-    plt.show()
-    print('num_SNR =',num_SNR_total)
-    print('ana_SNR =',ana_SNR_total)
-    print('halfpoint is', half_v_h)
-    print('time',t_diff,'+-',t_diff_var)
-    print('angle',angle,'+-',angle_var)
+    fig = plt.figure(figsize = (12,10),constrained_layout = True)
+    axes = fig.subplots(2,2).flatten()
+    axes[0].loglog(nu,noise_smooth_h)
+    axes[0].set_title('Hanford smoothed noise')
+    axes[0].set_xlim(20,1500)
+    axes[1].loglog(nu,noise_smooth_l)
+    axes[1].set_title('Livingston smoothed noise')
+    axes[1].set_xlim(20,1500)
+    axes[2].plot(m_h_shifted)
+    axes[2].set_title('Hanford matched filer')
+    axes[3].plot(m_l_shifted)
+    axes[3].set_title('Livingston matched filer')
+    plt.suptitle(f'Event is {ename}')
+    plt.savefig(f'ana_{ename}.png')
+    # plt.show()
+    # plt.plot(m_h)
+    # plt.xlim([np.argmax(np.abs(m_h))-50,np.argmax(np.abs(m_h))+50])
+    # plt.show()
+    print('\nEvent is ',ename)
+    print('Hanford Numerical SNR =',num_SNR_h)
+    print('Hanford Analytic SNR =',ana_SNR_h)
+    print('Livingston Numerical SNR =',num_SNR_l)
+    print('Livingston Analytic SNR =',ana_SNR_l)
+    print('Total Numerical SNR =',num_SNR_total)
+    print('Total Analytic SNR =',ana_SNR_total)
+    print('Hanford Half-weight frequency is: ', half_v_h)
+    print('Livingston Half-weight frequency is: ', half_v_l)
+    print('Time difference is: ',t_diff,'+-',t_diff_var)
+    print('Angle of the source is: ',angle,'+-',angle_var)
 
 
-ligo_analysis(0)
+for i in range(4):
+    ligo_analysis(i)
